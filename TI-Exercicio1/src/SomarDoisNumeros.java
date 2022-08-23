5454d52d63e526a3598129fef5e9fe6b6a91ca4c
rafaelnunesneumann <107643462+rafaelnunesneumann@users.noreply.github.com>
2022-08-23 17:13:55 -0300

    Add files via upload

---
+++ TI-Exercicio1/src/SomarDoisNumeros.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class SomarDoisNumeros {
+	
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		//Declaração de variáveis
+		int num1, num2, soma;
+		
+		//Leituras
+		System.out.println("Digite um número");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro número");
+		num2 = sc.nextInt();
+		
+		//Somar
+		soma = num1 + num2;
+		
+		//Mostrar na tela
+		System.out.println("Soma: " + soma);
+	}
+
+}
